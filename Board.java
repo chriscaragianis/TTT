f3546d95f3fe240b8bbb0710b8e269b8ba96cef4
houyouheng <houyouheng@hotmail.com>
2016-03-07 11:17:02 -0500

    GameRunner Logic.

--- Board.java
+++ Board.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Board{
 
-  private static String[][] board;
+  private String[][] board;
 
-  public static void board(int length){
+  public void startBoard(int length){
     board = new String[length][length];
     for(int i = 0; i < length; i++){
       for(int j = 0; j < length; j++){
@@ -13,7 +13,7 @@ class Board{
     }
   }
 
-  public static boolean isFull(){
+  public boolean isFull(){
     for(int i = 0; i < board.length; i++){
       for(int j = 0; j < board[i].length; j++){
         if(board[i][j] == "-"){
@@ -24,7 +24,7 @@ class Board{
     return true;
   }
 
-  public static void makeBoard(){
+  public void makeBoard(){
     System.out.println("-------");
     System.out.println("| | | |");
     System.out.println("-------");
@@ -34,7 +34,7 @@ class Board{
     System.out.println("-------");
   }
 
-  public static void printBoard() {
+  public void printBoard() {
     System.out.println("-------------");
     for (int i = 0; i < 3; i++) {
       System.out.print("| ");
--- GameRunner.java
+++ GameRunner.java
@@ -1,7 +1,21 @@
 class GameRunner{
 
+  public void checkForWin(){
+    for(int i = 0; i < board.length; i++){
+      int position = 0;
+      if(board[i][position] == "X" && board[i][position + 1] == "X" &&
+        board[i][position + 2] == "X"){
+        System.out.println("X Wins!");
+      }
+      else if(board[i][position] == "O" && board[i][position + 1] == "O" &&
+        board[i][position + 2] == "O"){
+        System.out.println("X Wins!");
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    Board myBoard = new board(3);
+    Board myBoard = new Board();
     myBoard.startBoard(3);
     myBoard.makeBoard();
     myBoard.printBoard();
